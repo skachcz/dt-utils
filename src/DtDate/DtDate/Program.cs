f46e83a0818fb9a00730f8acf6a7df301599f254
Vladimir Skach <vladimir.skach@astrosatmedia.cz>
2017-02-01 13:44:07 +0100

    DtDate utility added

---
+++ src/DtDate/DtDate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DtLib;
+
+namespace DtDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DtLib.ArgsData data = new ArgsData(args);
+
+            string format = "{0:yyyy-MM-dd-HH-mm}";
+
+            if (data.NamedArgs.ContainsKey("format"))
+            {
+                format = data.NamedArgs["format"].Value;
+            }
+
+            try
+            {
+                Console.WriteLine(String.Format(format, DateTime.Now));
+            }
+
+            catch(System.FormatException exc)
+            {
+                Console.WriteLine(Messages.getText(Messages.ERR_ARG_INVALID_VALUE,
+                    new object[] { "-format", "Visit: https://msdn.microsoft.com/en-us/library/8kb3ddd4(v=vs.110).aspx" }));
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
