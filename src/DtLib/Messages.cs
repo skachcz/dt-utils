c80fdcdd0c1c55064226eea272aba0728145c3a4
Vladimir Skach <vladimir.skach@gmail.com>
2017-08-03 13:53:38 +0200

    creator string

--- src/DtLib/Messages.cs
+++ src/DtLib/Messages.cs
@@ -8,6 +8,8 @@ namespace DtLib
 {
     public static class Messages
     {
+        public const string INFO_UTILITY_NAME = "Dt Utils - Vladimir Skach (C) 2017";
+
         public const string ERR_FILE_NOT_FOUND = "File {0} not found";
         public const string ERR_ARG_IS_MANDATORY = "Parameter {0} has to be set. {1}";
         public const string ERR_ARG_INVALID_VALUE = "Parameter {0} has some invalid values. {1}";
