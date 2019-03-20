15dc6d315318527466bafe9bfb8320bf5ef59fb5
Vladimir Skach <vladimir.skach@gmail.com>
2019-03-20 10:32:35 +0100

    dtlist fixes

--- src/DtLib/Messages.cs
+++ src/DtLib/Messages.cs
@@ -10,7 +10,7 @@ namespace DtLib
     {
         public const string INFO_UTILITY_NAME = "Dt Utils - Vladimir Skach (C) 2017";
 
-        public const string ERR_FILE_NOT_FOUND = "File {0} not found";
+        public const string ERR_FILE_NOT_FOUND = "File or directory {0} not found";
         public const string ERR_ARG_IS_MANDATORY = "Parameter {0} has to be set. {1}";
         public const string ERR_ARG_INVALID_VALUE = "Parameter {0} has some invalid values. {1}";
         public const string ERR_ARG_WRONG_NUMBER = "The number of parameters is invalid. {0}";
--- src/DtList/Program.cs
+++ src/DtList/Program.cs
@@ -23,25 +23,32 @@ namespace Dtlist
                 path = Directory.GetCurrentDirectory();
             }
 
-            DateDirectory dir = new DateDirectory(path, "*.*");
-
-            Console.WriteLine("{0,-15}\t{1,-15}\t{2,-15}\t{3}",
-                "created", "accessed", "modified", "filename"
-                );
-
-            foreach (var f in dir.Files)
+            try
             {
-                Console.WriteLine("{0,-15}\t{1,-15}\t{2,-15}\t{3}", 
-                    f.DateCreated, f.DateAccessed, f.DateModified, f.Filename
+                DateDirectory dir = new DateDirectory(path, "*.*");
+
+                Console.WriteLine("{0,-19}\t{1,-19}\t{2,-19}\t{3}",
+                    "Created", "Accessed", "Modified", "Filename"
                     );
+
+                foreach (var f in dir.Files)
+                {
+                    Console.WriteLine("{0,-15}\t{1,-15}\t{2,-15}\t{3}",
+                        f.DateCreated, f.DateAccessed, f.DateModified, f.Filename
+                        );
+                }
             }
 
+            catch(System.IO.DirectoryNotFoundException)
+            {
+                Console.WriteLine(Messages.getText(Messages.ERR_FILE_NOT_FOUND,
+                new object[] { path } ));
 
-            Console.WriteLine("Press key");
-            Console.ReadLine();
+            }
+
+            // Console.WriteLine("Press key");
+            // Console.ReadLine();
         }
-
-
 
     }
 }
